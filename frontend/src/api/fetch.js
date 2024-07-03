3de915df7097e2926d01c70404c9e1864f184ef2
Esquiafo <leocapone@hotmail.com>
2024-07-03 00:37:04 -0300

    feat: remove log

--- frontend/src/api/fetch.js
+++ frontend/src/api/fetch.js
@@ -3,7 +3,6 @@ const API_BASE_URL = 'http://localhost:8080/api';
 export const fetchDestinationData = async () => {
     try {
         const response = await fetch(`${API_BASE_URL}/destination`);
-        console.log(response)
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
